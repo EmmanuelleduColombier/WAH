1911a5ad124dc6ccf560788262350ad0a53672f4
Tom Paganel <paganel.tom@gmail.com>
2021-11-16 15:16:22 +0100

    modify review policy

--- app/policies/review_policy.rb
+++ app/policies/review_policy.rb
@@ -1,11 +1,6 @@
 class ReviewPolicy < ApplicationPolicy
   attr_reader :user, :review
 
-  def initialize(user, review)
-    @user = user
-    @review = review
-  end
-
   def new?
     create?
   end

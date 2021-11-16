98b8b72953d60880e328a9223b971467f66f1b63
Stregold <stregold@gmail.com>
2021-11-16 11:20:00 +0100

    index_show_nex_create_methods_flats

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,14 +1,23 @@
 class FlatsController < ApplicationController
   def index
+    @flats = Flat.all
   end
 
   def show
+    @flat = Flat.find(params[:id])
   end
 
   def new
+    @flat = Flat.new
   end
 
   def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -21,5 +30,13 @@ class FlatsController < ApplicationController
   end
 
   def myflats
+    @restaurants = policy_scope(Restaurant).order(created_at: :desc)
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:title, :address, :room_type, :persons, :price, :owner_present, :has_pet, :has_computer,
+                                      :has_coffee, :description)
   end
 end

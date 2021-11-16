d243578034b7bd0f63bcdcd313da97489f285711
Benjamin McConnell <crosslegged@gmail.com>
2021-11-16 16:54:06 +0100

    reviews controller and views almost done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,24 @@
 class ReviewsController < ApplicationController
   def new
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new
   end
 
   def create
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new(review_params)
+    @review.booking = @booking
+    @flat = Flat.find(@booking.flat_id)
+    if @review.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end

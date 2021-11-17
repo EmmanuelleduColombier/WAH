43d89a1df37e999a82af2de967bc38b0393924ac
Tom Paganel <paganel.tom@gmail.com>
2021-11-17 16:22:57 +0100

    fox

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,12 +1,11 @@
 import flatpickr from "flatpickr";
 
 const initFlatpickr = () => {
-
   const dates = document.querySelector('#booking-form').dataset.dates
   const data = JSON.parse(dates)
 
   flatpickr(".datepicker", {
-    disable: data
+    disable: data,
   });
 }
 

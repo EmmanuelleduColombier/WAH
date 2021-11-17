be45b9ca35912d5c6b7a1081fad5df07aa053fd9
Tom Paganel <paganel.tom@gmail.com>
2021-11-17 13:15:36 +0100

    raise remove

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -16,7 +16,6 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(booking_params)
     authorize @booking
-    raise
     @booking.status = 'pending'
     @flat = Flat.find(params[:flat_id])
     @booking.flat_id = @flat.id
